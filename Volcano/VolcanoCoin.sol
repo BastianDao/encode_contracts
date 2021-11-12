fdf587466736cff2b03fc6150d7a9047ea69b334
BastianDao <scaso@knightpoint.com>
2021-11-12 10:25:29 -0500

    update volcano contract to track payments

--- Volcano/VolcanoCoin.sol
+++ Volcano/VolcanoCoin.sol
@@ -6,8 +6,22 @@ contract VolcanoCoin {
     uint256 public totalSupply = 10000;
     address private owner;
 
-    event SupplyIncrease(uint256 totalSupply);
+    struct Payment {
+        uint256 amount;
+        address recipient;
+    }
 
+    struct Wallet {
+        uint256 balance;
+        uint256 numberOfPayments;
+        Payment[] payments;
+    }
+
+    mapping(address => Wallet) public wallet;
+
+
+    event SupplyIncrease(uint256 totalSupply);
+    event TransferOccured(uint256 _amount, address _recipient);
 
     modifier isOwner() {
         require(msg.sender == owner, "Caller is not owner");
@@ -16,6 +30,25 @@ contract VolcanoCoin {
 
     constructor() {
         owner = msg.sender;
+        wallet[msg.sender].balance = totalSupply;
+    }
+
+    function transfer(uint256 _amount, address _recipient) public {
+        require(wallet[msg.sender].balance >= _amount, "not enough funds");
+
+        wallet[msg.sender].balance -= _amount;
+        wallet[_recipient].balance += _amount;
+        
+        Payment memory payment = Payment(_amount, _recipient);
+
+        wallet[msg.sender].payments.push(payment);
+        wallet[msg.sender].numberOfPayments++;
+
+        emit TransferOccured(_amount, _recipient);
+    }
+
+    function getAllUsersPayments(address _user) public view returns(Payment[] memory)  {
+        return wallet[_user].payments;
     }
 
     function increaseTotalSupply() public isOwner {
