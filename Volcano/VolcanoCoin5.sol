f4cf8e5696a6fc0965f3741b1c870f4a42b064ff
BastianDao <caso.3@osu.edu>
2021-12-21 10:07:29 -0500

    complete Homework 10

--- Volcano/VolcanoCoin5.sol
+++ Volcano/VolcanoCoin5.sol
@@ -7,15 +7,26 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract VolcanoCoin is ERC20, Ownable {
     constructor(uint256 initialSupply) ERC20("Volcano", "VOL") {
         _mint(owner(), initialSupply);
+        administrator = msg.sender;
     }
+
+    address administrator;
 
     event TransferOccured(uint256 _amount, address _recipient);
     event SupplyIncrease(uint256 totalSupply);
+    
+    uint private paymentCounter;
+    enum PaymentType { Unknown, BasicPayment, Refund, Dividend, GroupPayment }
+    PaymentType constant defaultPayment = PaymentType.Unknown;
 
     struct Payment {
+        uint paymentID;
+        uint256 timestamp;
         uint256 amount;
         address sender;
         address recipient;
+        string comment;
+        PaymentType paymentType;
     }
 
     mapping(address => Payment[]) public userPayments;
@@ -35,12 +46,63 @@ contract VolcanoCoin is ERC20, Ownable {
     function transfer(address _recipient, uint256 _amount) public override returns (bool){
         _transfer(msg.sender, _recipient, _amount);
 
-        Payment memory payment = Payment(_amount, msg.sender, _recipient);
-
-        userPayments[msg.sender].push(payment);
+        // Payment memory payment = Payment(_amount, msg.sender, _recipient);
+        recordPayment(_recipient, _amount);
         emit TransferOccured(_amount, _recipient);
 
         return true;
     }
 
+    function recordPayment(address _recipient, uint256 _amount) internal {
+        userPayments[msg.sender].push(Payment(++paymentCounter, _amount, _recipient, defaultPayment, "", block.timestamp));
+    }
+
+    function viewPayments() public view returns(Payment[] memory payments){
+        return userPayments[msg.sender];
+    }
+
+    function searchPayments(address _payer, uint256 _paymentId, uint _paymentType) private view returns (Payment[] storage, Payment memory, uint) {
+        require(_paymentType <= paymentTypeEnumCount, "PaymentType does not exist");
+        
+        Payment[] storage userPayments = payments[_payer];
+        require(userPayments.length > 0, "user has made no payments");
+
+        Payment memory returnPayment;
+        uint index;
+
+        for (uint i=0; i< userPayments.length; i++) {
+            Payment memory payment = userPayments[i];
+
+            if (_paymentId == payment.paymentId) {
+                index = i;
+                returnPayment = payment;
+                break;
+            }
+        }
+
+        require(returnPayment.paymentId != 0, "Could not find payment");
+
+        return (userPayments, returnPayment, index);
+    }
+
+    function updatePayment(uint256 _paymentId, uint _paymentType, string memory _comment) public {
+        (Payment[] storage userPayments, Payment memory payment, uint i) = searchPayments(_payer, _paymentId, _paymentType);(msg.sender, _paymentId, _paymentType);
+        
+        payment.paymentType = PaymentType(_paymentType);
+        payment.comment = bytes(payment.comment).length == 0 ? _comment : string(abi.encode(payment.comment, "; ", _comment));
+        userPayments[i] = payment;
+
+        payments[msg.sender] = userPayments;
+    }
+
+    function updatePaymentAdmin(address _payer, uint256 _paymentId, uint8 _paymentType) public {
+        require(msg.sender == administrator, "Admin only action!");        
+        (Payment[] storage userPayments, Payment memory payment, uint i) = searchPayments(_payer, _paymentId, _paymentType);(_payer, _paymentId, _paymentType);        
+
+        payment.paymentType = PaymentType(_paymentType);
+        payment.comment = bytes(payment.comment).length == 0 ? string(abi.encode("updated by ", string(abi.encode(administrator)))) : string(abi.encodePacked(payment.comment, "; updated by ", string(abi.encodePacked(administrator))));
+        userPayments[i] = payment;
+
+        payments[_payer] = userPayments;
+    }
 }
